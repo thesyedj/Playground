b4e754d21ffce6e9ebb1abbc9bb42d47e7e8386b
thesyedj <58871935+thesyedj@users.noreply.github.com>
2020-05-04 03:01:45 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+int ID;
+  int Totalmarks;
+  std::cin>> ID;
+  std::cin>> Totalmarks;
+  std::cout<<"Id : "<<ID;
+  std::cout<<"\nTotalmarks : "<<Totalmarks;
+}
---
+++ Naming  Ceremony/Main.java
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+char a[10];
+
+cin>>a;
+
+cout<<a;
+
+}
